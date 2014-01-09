033953cc390afa1727ce468e734113bb10f5a8ff
CharleZhu <zhuchuantao1990@126.com>
2014-01-09 15:08:16 +0800

    modify

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,6 +2,7 @@
 class Teacher < ActiveRecord::Base
   attr_protected :authentications
   has_many :school_classes, :dependent => :destroy
+  has_many :question_packages, :dependent => :destroy
   STATUS = {:YES => 0, :NO => 1}
   STATUS_NAME = {0 => '正常', 1 => "失效"}
 end

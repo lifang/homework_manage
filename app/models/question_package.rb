1488c1dbd16b4560d1e848962bffcb5b95a12868
CharleZhu <zhuchuantao1990@126.com>
2014-01-09 15:15:08 +0800

    modify

--- app/models/question_package.rb
+++ app/models/question_package.rb
@@ -1,7 +1,4 @@
 #encoding: utf-8
 class QuestionPackage < ActiveRecord::Base
   attr_protected :authentications
-  def self.get_questions question_package
-
-  end
 end
--- app/models/student_answer_record.rb
+++ app/models/student_answer_record.rb
@@ -1,10 +1,12 @@
 #encoding: utf-8
 class StudentAnswerRecord < ActiveRecord::Base
   attr_protected :authentications
-  STATUS = {:NEW => 0, :DEALING => 1, :FINISH => 2}
-  STATUS_NAME = {0 => "新任务", 1 => "进行中", 2 => "完成"}
+  STATUS = {:DEALING => 0, :FINISH => 1}
+  STATUS_NAME = {0 => "进行中", 1 => "完成"}
 
   def self.get_daily_tasks school_class, student_id
+    #dealing_tasks_sql_str = "select "
+    #dealing_tasks = Student.find_by_sql dealing_tasks_sql_str
     dealing_tasks = nil
     unfinish_tasks = nil
     return_into = {:dealing_tasks => dealing_tasks, :unfinish_tasks => unfinish_tasks}

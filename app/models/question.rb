fa694fcce190a1f4415ed0355773422dd571f0f8
CharleZhu <zhuchuantao1990@126.com>
2014-01-09 14:42:46 +0800

    modify

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 #encoding: utf-8
 class Question < ActiveRecord::Base
   attr_protected :authentications
+  TYPES = {:LISTENING => 0, :READING => 1}
+  TYPES_NAME = {0 => "听力", 1 => "朗读"}
 end
--- app/models/question_package.rb
+++ app/models/question_package.rb
@@ -1,4 +1,7 @@
 #encoding: utf-8
 class QuestionPackage < ActiveRecord::Base
   attr_protected :authentications
+  def self.get_questions question_package
+
+  end
 end

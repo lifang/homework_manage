b082621da58636831d3fd2c23eec0f41706abead
wangping <525399584@qq.com>
2014-01-20 10:14:47 +0800

    adsad

--- app/helpers/teachers_helper.rb
+++ app/helpers/teachers_helper.rb
@@ -1,7 +1,6 @@
 module TeachersHelper
   def current_teacher
     sql="select t.id,t.user_id,t.password,t.email,t.status,t.types,t.last_visit_class_id,u.name,u.avatar_url from teachers t left join users u on t.user_id=u.id where t.user_id=?"
-    p session[:user_id]
     params_arr =[sql,session[:user_id]]
     @teacher ||=Teacher.find_by_sql(params_arr)[0]    
     @teacher

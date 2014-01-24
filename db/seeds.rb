9366b47cbd83a25022f87d7596486da1b24f1462
lifang <santhe_2004@126.com>
2014-01-24 10:12:24 +0800

    update detial

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # id, name, nickname, avatar_url, alias_name, qq_uid, status, last_visit_class_id, register_status, created_at, updated_at
 
-num = 1 * 100
-TeachingMaterial.create(:id => num+1,:name => "牛津英语")
-TeachingMaterial.create(:id => num+1,:name => "新目标英语")
-TeachingMaterial.create(:id => num+1,:name => "新概念英语")
+TeachingMaterial.create(:id => 1,:name => "牛津英语")
+TeachingMaterial.create(:id => 2,:name => "新目标英语")
+TeachingMaterial.create(:id => 3,:name => "新概念英语")
 
 TeachingMaterial.transaction do
   TeachingMaterial.find_each do |tm|

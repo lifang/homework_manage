aa9f64d5be1c55ad1ef8cd8b89541c775e9b01cd
CharleZhu <zhuchuantao1990@126.com>
2014-01-25 11:34:02 +0800

    modify

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -50,7 +50,7 @@ class HomeworksController < ApplicationController
   def publish_question_package
     question_package_id = params[:question_package_id]
     school_class_id = params[:school_class_id]
-    end_time = params[:end_time]
+    end_time = params[:end_time].to_s + " 23:59:59"
     teacher = Teacher.find_by_id cookies[:teacher_id]
     question_package = QuestionPackage.find_by_id question_package_id
     @school_class = SchoolClass.find_by_id school_class_id

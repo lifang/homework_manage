aecef41ff1e5bf6b6fc1e056244debd4a7ba73e9
CharleZhu <zhuchuantao1990@126.com>
2014-02-13 17:26:06 +0800

    modify

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,7 +5,7 @@ class Message < ActiveRecord::Base
   belongs_to :micropost
   STATUS = {:NOMAL => 0, :READED => 1} #未阅读 0  已阅读
   PER_PAGE = 10
-  def self.add_messages(micropost_id, reciver_id, reciver_types, sender_id, 
+  def self.add_messages(micropost_id, reciver_id, reciver_types, sender_id,
       sender_types, content, school_class_id)
     sender = User.find_by_id sender_id.to_i
     micropost = Micropost.find_by_id micropost_id.to_i
@@ -15,7 +15,7 @@ class Message < ActiveRecord::Base
         count_msg += 1
         m_content = "[[" + sender.name + "]]回复了您的消息：;||;" + content
         Message.create(:user_id => reciver_id, :content => m_content, :micropost_id => micropost_id,
-          :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
+                       :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
       end
       follow_microposts = FollowMicropost.find_all_by_micropost_id(micropost_id.to_i)
       follow_users = follow_microposts.collect {|i| i.user_id }
@@ -25,13 +25,13 @@ class Message < ActiveRecord::Base
                        :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
       end
       if follow_microposts.any?
-          follow_users.each do |u_id|
-            unless sender_id.to_i == u_id
-                f_content = "[[" + sender.name + "]]回复了您关注的消息：;||;" + content
-                Message.create(:user_id => u_id, :content => f_content, :micropost_id => micropost_id,
-                  :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
-            end
+        follow_users.each do |u_id|
+          unless sender_id.to_i == u_id
+            f_content = "[[" + sender.name + "]]回复了您关注的消息：;||;" + content
+            Message.create(:user_id => u_id, :content => f_content, :micropost_id => micropost_id,
+                           :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
           end
+        end
       end
     end
   end
@@ -39,11 +39,11 @@ class Message < ActiveRecord::Base
   #获取群我的当前班级的的消息
   def self.get_my_messages school_class, user_id
     messages = Message.joins('LEFT JOIN users u ON messages.sender_id = u.id').
-      select("messages.id, messages.content, messages.user_id, messages.created_at, u.name sender_name,
+        select("messages.id, messages.content, messages.user_id, messages.created_at, u.name sender_name,
      u.avatar_url sender_avatar_url, messages.micropost_id ").
-      order("messages.created_at DESC").
-      where("user_id = ? and school_class_id = ? and status = ?", user_id, school_class.id,
-      Message::STATUS[:NOMAL])
+        order("messages.created_at DESC").
+        where("user_id = ? and school_class_id = ? and status = ?", user_id, school_class.id,
+              Message::STATUS[:NOMAL])
 
   end
 end

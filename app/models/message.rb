1eccacc2a46a0af16e73c03910632d0d656d3f4c
CharleZhu <zhuchuantao1990@126.com>
2014-02-13 15:37:30 +0800

    modify

--- app/models/message.rb
+++ app/models/message.rb
@@ -18,15 +18,37 @@ class Message < ActiveRecord::Base
       follow_microposts = FollowMicropost.find_all_by_micropost_id(micropost_id.to_i)
       if follow_microposts.any?
         follow_users = follow_microposts.collect {|i| i.user_id }
-        follow_users.each do |u_id|
-          unless u_id == reciver_id.to_i || u_id == sender_id
-            f_content = "[[" + sender.name + "]]回复了您关注的消息：;||;" + content
-            Message.create(:user_id => u_id, :content => f_content, :micropost_id => micropost_id,
-              :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
+        sql_str = "select r.id from reply_microposts r where micropost_id = #{micropost.id}"
+        reply_micropost_ids = ReplyMicropost.find_by_sql sql_str
+        if follow_users.include?(reciver_id.to_i)
+          follow_users.each do |u_id|
+            unless u_id == reciver_id.to_i
+              if sender_id.to_i != reciver_id.to_i
+                f_content = "[[" + sender.name + "]]回复了您关注的消息：;||;" + content
+                Message.create(:user_id => u_id, :content => f_content, :micropost_id => micropost_id,
+                  :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
+              else
+                e_content = "[[" + sender.name + "]]回复了您：;||;" + content
+                Message.create(:user_id => u_id, :content => e_content, :micropost_id => micropost_id,
+                               :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
+              end
+            end
+          end
+        else
+          if sender_id.to_i != reciver_id.to_i
+            e_content = "[[" + sender.name + "]]回复了您：;||;" + content
+            Message.create(:user_id => u_id, :content => e_content, :micropost_id => micropost_id,
+                           :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
           end
         end
+      else
+          if sender_id.to_i != reciver_id.to_i
+            e_content = "[[" + sender.name + "]]回复了您：;||;" + content
+            Message.create(:user_id => u_id, :content => e_content, :micropost_id => micropost_id,
+                           :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
+          end
       end
-    end    
+    end
   end
 
   #获取群我的当前班级的的消息

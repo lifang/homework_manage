27e6df1167e710bed4090696db292ee23f0fb2a0
CharleZhu <zhuchuantao1990@126.com>
2014-02-13 16:15:44 +0800

    modify

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,44 +8,29 @@ class Message < ActiveRecord::Base
   def self.add_messages(micropost_id, reciver_id, reciver_types, sender_id, 
       sender_types, content, school_class_id)
     sender = User.find_by_id sender_id.to_i
-    micropost = Micropost.find_by_id micropost_id.to_i 
+    micropost = Micropost.find_by_id micropost_id.to_i
+    count_msg = 0
     if sender
       unless sender_id.to_i == reciver_id.to_i or sender_id.to_i == micropost.user_id
+        count_msg += 1
         m_content = "[[" + sender.name + "]]回复了您的消息：;||;" + content
         Message.create(:user_id => reciver_id, :content => m_content, :micropost_id => micropost_id,
           :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
       end
       follow_microposts = FollowMicropost.find_all_by_micropost_id(micropost_id.to_i)
+      follow_users = follow_microposts.collect {|i| i.user_id }
+      if follow_users.include?(reciver_id.to_i)
+        e_content = "[[" + sender.name + "]]回复了您：;||;" + content
+        Message.create(:user_id => reciver_id, :content => e_content, :micropost_id => micropost_id,
+                       :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
+      end
       if follow_microposts.any?
-        follow_users = follow_microposts.collect {|i| i.user_id }
-        sql_str = "select r.id from reply_microposts r where micropost_id = #{micropost.id}"
-        reply_micropost_ids = ReplyMicropost.find_by_sql sql_str
-        if follow_users.include?(reciver_id.to_i)
           follow_users.each do |u_id|
             unless u_id == reciver_id.to_i
-              if sender_id.to_i != reciver_id.to_i
                 f_content = "[[" + sender.name + "]]回复了您关注的消息：;||;" + content
                 Message.create(:user_id => u_id, :content => f_content, :micropost_id => micropost_id,
                   :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
-              else
-                e_content = "[[" + sender.name + "]]回复了您：;||;" + content
-                Message.create(:user_id => u_id, :content => e_content, :micropost_id => micropost_id,
-                               :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
-              end
             end
-          end
-        else
-          if sender_id.to_i != reciver_id.to_i
-            e_content = "[[" + sender.name + "]]回复了您：;||;" + content
-            Message.create(:user_id => reciver_id, :content => e_content, :micropost_id => micropost_id,
-                           :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
-          end
-        end
-      else
-          if sender_id.to_i != reciver_id.to_i
-            e_content = "[[" + sender.name + "]]回复了您：;||;" + content
-            Message.create(:user_id => reciver_id, :content => e_content, :micropost_id => micropost_id,
-                           :school_class_id => school_class_id, :status => STATUS[:NOMAL], :sender_id => sender.id)
           end
       end
     end

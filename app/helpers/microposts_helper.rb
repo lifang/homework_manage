bee4baaee1789111af7eb4b530a0c23b3588546e
chenlong <KLoong777@163.com>
2014-02-20 17:18:19 +0800

    cl

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -2,7 +2,6 @@
 module MicropostsHelper
   require 'net/http'
   def jpush_parameter messages,receivervalue
-    message = "\"#{message}\""
     sendno = 1001
     receivertype = 3
     mastersecret = "902d3da3dc9366734a84ee21"
@@ -10,10 +9,6 @@ module MicropostsHelper
     code = Digest::MD5.hexdigest(input)
     msg_content =  "{\"n_title\":\"1111222\",\"n_content\":#{messages},\"n_extras\":{\"class_id\":\"2\"} }"
     msg_content = {"n_content" => "#{messages}","n_title"=> "2iidid","extras"=>{"class_id"=> "2"}}.to_json()
-#    msg_content = {"n_content" => "#{messages}"}.to_json()  #,"title"=>"iiiii","extras"=>{"class_id"=>"2"} }.to_json()
-    p msg_content
-#    msg_content = {"n_title":"hello","n_content":"hello zhe tian","n_extras":{"n_url":"","n_type":"1"}}
-
     map = Hash.new
     map.store("sendno", sendno)
     map.store("app_key", "26e6b3d44da78ea902c7fac9")

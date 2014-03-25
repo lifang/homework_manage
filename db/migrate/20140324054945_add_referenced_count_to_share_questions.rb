d77dcdae899f95f3fed0738eea0da74cec712244
amanda <mengfanling825@163.com>
2014-03-25 11:49:27 +0800

    add columns to share questions

--- db/migrate/20140324054945_add_referenced_count_to_share_questions.rb
+++ db/migrate/20140324054945_add_referenced_count_to_share_questions.rb
@@ -1,5 +1,5 @@
 class AddReferencedCountToShareQuestions < ActiveRecord::Migration
   def change
-    add_column :share_questions, :referenced_count, :integer  #被引用次数
+    add_column :share_questions, :referenced_count, :integer, :default => 0  #被引用次数
   end
 end

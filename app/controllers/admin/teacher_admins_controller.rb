280f197439069e9f1b6052ab4ccf145042f693d5
chenlong <KLoong777@163.com>
2014-04-09 13:52:17 +0800

    cl

--- app/controllers/admin/teacher_admins_controller.rb
+++ app/controllers/admin/teacher_admins_controller.rb
@@ -1,5 +1,29 @@
 #encoding: utf-8
 class Admin::TeacherAdminsController < ApplicationController
+  layout "admin"
   def index
+    p 111111111111
+  end
+
+  #  新建教师
+  def create
+    teacher_name = params[:teacher_name]
+    teacher_email = params[:teacher_email]
+    teacher_exit = Teacher.find_by_email teacher_email
+    avatar_url = "/assets/default_avater.jpg"
+    if teacher_exit
+      @status = 0
+      @notice = "邮箱已存在！"
+    else
+      
+    end
+  end
+  #  重设密码
+  def update
+
+  end
+  #  是否停用
+  def is_enable
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,11 @@ HomeworkManage::Application.routes.draw do
       end
     end
 
+    resources :teacher_admins do
+      collection do
+      end
+    end
+
     resources :systems do
       collection do
         post :reset_password, :disable_enable_account
@@ -136,7 +141,7 @@ HomeworkManage::Application.routes.draw do
           :new_reading_or_listening, :share_question, :delete_question,
           :search_b_tags, :add_b_tags,:save_branch_tag, :set_question_time
         post :create_time_limit, :save_listening, :save_reading, :set_question_time, :delete_branch, :upload_voice,
-              :update_listening
+          :update_listening
       end
       member do
         get :new_index,:show_wanxin,:create_wanxin,:create_paixu,

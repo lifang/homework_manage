853d05d62b170d66f3d0802c247d0bf828b7a14a
CharleZhu <zhuchuantao1990@126.com>
2014-04-09 16:25:17 +0800

    modify

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,4 +13,9 @@ class UserMailer < ActionMailer::Base
     @url  = '58.240.210.42:3004'   #服务器地址
     mail(to: @email, subject: '重置密码')
   end
+
+  def apply_quota_consumptions(email, sender_name, school_name, number, type)
+    @email, @content, @type_name, @school_name, @number, @sender_name = email, content, Teacher::TYPES_NAME[type], school_name, number, sender_name  
+    mail(to: @email, subject: '申请学生配额')
+  end  
 end

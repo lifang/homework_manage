c50f10d5f4bc4bc94868634d3ab6b60f695c331c
CharleZhu <zhuchuantao1990@126.com>
2014-06-09 10:08:25 +0800

    modify

---
+++ app/assets/javascripts/dictations/main.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+$(function(){
+	$(".assignment_btn_a").click(function(){
+		var year = parseInt($(".nian").find("span").text());
+		var month = parseInt($(".yue").find("span").text());
+		
+	})
+})
